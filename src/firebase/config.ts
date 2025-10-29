6fa94fe220041fd439d1494e2284e030619f5a7a
Mir Abrar Ali <alimirabrar@gmail.com>
2025-10-29 07:54:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-846341198-1cb7b",
+  "appId": "1:39896202643:web:6d7402107b1d7b7f9b3c66",
+  "apiKey": "AIzaSyAVbbpnCILRyx1kN1wEAz5PNoHFweX-OJc",
+  "authDomain": "studio-846341198-1cb7b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "39896202643"
+};
