a039f120a8c51a8cf18e744d92d46557c69f15db
Mir Abrar Ali <alimirabrar@gmail.com>
2025-11-11 14:05:31 +0000

    Try fixing this error: `Console Error: GenkitError: NOT_FOUND: Model 'ge

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,11 +4,12 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      models: {
-        'gemini-1.5-flash': {
-          model: 'gemini-1.5-flash',
-        },
-      },
+      models: [
+        {
+          name: 'gemini-1.5-flash',
+          model: 'gemini-1.5-flash-latest',
+        }
+      ],
     }),
   ],
   model: 'gemini-1.5-flash',
