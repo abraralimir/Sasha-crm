08db7391e00c5e47f058b612059c981eb1c3934e
Mir Abrar Ali <alimirabrar@gmail.com>
2025-11-11 12:12:02 +0000

    17:39:38.174 Running build in Washington, D.C., USA (East) – iad1 17:39:

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,5 +1,7 @@
 import {createApiHandlerV1} from '@genkit-ai/next';
 import '@/ai';
 
+export const runtime = 'nodejs';
+
 export const GET = createApiHandlerV1();
 export const POST = createApiHandlerV1();
